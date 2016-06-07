f2b5b0e1ecfc2842794136016025cdd035c67810
Andrey Rubayko <tograilx@gmail.com>
2016-06-07 08:35:50 +0300

    changed credentials

--- subscribe.php
+++ subscribe.php
@@ -1,7 +1,7 @@
 <?php
 	$message = "E-mail: ".$_POST['subemail'];
-    $to='piratarip@gmail.com'; // Адрес получателя
-    $subj = "E-mail subscription rquest";
+    $to='tograilx@gmail.com'; // Адрес получателя
+    $subj = "E-mail subscription request";
     $headers = "Content-Type: text/html; charset=utf-8\r\n";
     $headers .= "From: Steady Works.pro\r\n";
     mail($to, $subj, $message, $headers);
