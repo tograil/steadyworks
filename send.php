6880a44de4a1e82483c71204d73c1bb9507ee49a
Andrey Rubayko <tograilx@gmail.com>
2016-06-07 08:46:54 +0300

    changed e-mail

--- send.php
+++ send.php
@@ -1,6 +1,6 @@
-<?php
+﻿<?php
 	$message = "E-mail: ".$_POST['email']."<br>Name: ".$_POST['name']."<br>Company: ".$_POST['company']."<br>Message: ".$_POST['message'];
-    $to='piratarip@gmail.com'; // Адрес получателя
+    $to='tograilx@gmail.com'; // Адрес получателя
     $subj = "Quote Request";
     $headers = "Content-Type: text/html; charset=utf-8\r\n";
     $headers .= "From: Steady Works.pro\r\n";
